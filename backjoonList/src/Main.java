b8d29eff60a2c6f861eca085a18e406e8d30aab5
soumunda8 <soumunda8@naver.com>
2023-08-05 20:36:15 +0900

    '윤년'

--- backjoonList/src/Main.java
+++ backjoonList/src/Main.java
@@ -4,21 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-
-        if(num >= 90) {
-            System.out.println("A");
-        } else if(num >= 80) {
-            System.out.println("B");
-        } else if(num >= 70) {
-            System.out.println("C");
-        } else if(num >= 60) {
-            System.out.println("D");
-        } else {
-            System.out.println("F");
-        }
-
     }
 
 }
